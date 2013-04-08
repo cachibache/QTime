c32a73731c2abb98bf048333c19ebb322adb9e16
Alex Hazoury <a.hazoury@gmail.com>
2013-04-08 17:21:36 +0100

    fixed views and modified previous migrations to set default values to table fields

--- db/migrate/20130408105749_add_number_of_true_to_question.rb
+++ db/migrate/20130408105749_add_number_of_true_to_question.rb
@@ -1,5 +1,5 @@
 class AddNumberOfTrueToQuestion < ActiveRecord::Migration
   def change
-    add_column :questions, :number_of_true, :integer
+    add_column :questions, :number_of_true, :integer, :default => 0
   end
 end
