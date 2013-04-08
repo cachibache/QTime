285f7d89397ddf844582e9d0ed4a109f0a6e011e
Alex Hazoury <a.hazoury@gmail.com>
2013-04-08 13:39:09 +0100

    added Questions index, new and create controllers

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,10 +1,19 @@
 class QuestionsController < ApplicationController
+
   def index
+    @questions = Question.all
   end
 
   def new
+    @question = Question.new
   end
 
   def create
+    @question = Question.new
+    if @question.save
+      redirect_to new_question_path
+    else
+      render :new
+    end
   end
 end
