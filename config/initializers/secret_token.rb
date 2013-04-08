de1a05767d170720e53ff4871e3cc2a07882b0eb
Alex Hazoury <a.hazoury@gmail.com>
2013-04-08 11:10:36 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QTime::Application.config.secret_token = '943db5480122740230045fe7b710b96c7b673d5b9724c4e4f88f6c92151401da8024f550fdd868521a61efb849a7fe251a6706dbebb20b9b74c2ae8de0483aba'
