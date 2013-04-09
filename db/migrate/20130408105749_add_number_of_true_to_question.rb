721cacb643b2715b6caa308ea7b8bfe7bb37a1ef
Alex Hazoury <a.hazoury@gmail.com>
2013-04-09 13:49:42 +0100

    updated files to be tracked

--- db/migrate/20130408105749_add_number_of_true_to_question.rb
+++
@@ -1,5 +0,0 @@
-class AddNumberOfTrueToQuestion < ActiveRecord::Migration
-  def change
-    add_column :questions, :answer, :string, :null => false
-  end
-end
--- db/migrate/20130408105813_add_number_of_false_to_question.rb
+++
@@ -1,5 +0,0 @@
-class AddNumberOfFalseToQuestion < ActiveRecord::Migration
-  def change
-    add_column :questions, :false, :integer, :default => 0
-  end
-end
