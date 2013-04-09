1102ca718c3b454664733c1105147333ab8cd487
Alex Hazoury <a.hazoury@gmail.com>
2013-04-09 15:25:08 +0100

    fixed tests

--- test/functional/statements_controller_test.rb
+++ test/functional/statements_controller_test.rb
@@ -12,26 +12,15 @@ class StatementsControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should get create" do
-    get :create
-    assert_response :success
-  end
-
-  test 'can create new statement' do
-    assert_equal 0, Statement.count
-    post :create, { :statement => {:statement => "some statement"} }
-    assert_equal 1, Statement.count
-  end
-
   test 'can create statement with false value' do
     assert_equal 0, Statement.count
-    post :create, { :statement => {:true_or_false => "false"} }
+    post :create, { :statement => {:statement => "some statement", :true_or_false => "false"} }
     assert_equal 1, Statement.count
   end
 
   test 'can create statement with true value' do
     assert_equal 0, Statement.count
-    post :create, { :statement => {:true_or_false => "true"} }
+    post :create, { :statement => {:statement => "some statement", :true_or_false => "true"} }
     assert_equal 1, Statement.count
   end
 
