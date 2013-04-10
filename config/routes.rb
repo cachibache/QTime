44ce7af286b301a31a871d96f4418ba493f6822d
Alex Hazoury <a.hazoury@gmail.com>
2013-04-10 15:29:42 +0100

    tiddy up

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,9 @@ QTime::Application.routes.draw do
   #   end
   # end
 
-  match 'auth/twitter/callback', :to => 'sessions#create'
-  match 'auth/failure', to: => redirect('/')
-  match 'signout', :to => 'sessions#destroy', :as => 'signout'
+  match 'auth/twitter/callback', to: 'sessions#create'
+  match 'auth/failure', to: redirect('/')
+  match 'signout', to: 'sessions#destroy', as: 'signout'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
