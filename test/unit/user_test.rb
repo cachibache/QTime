0098b649b700075d7cc54e0777300843ebaaf034
Alex Hazoury <a.hazoury@gmail.com>
2013-04-12 16:22:32 +0100

    ran annotate gem

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  provider   :string(255)
+#  uid        :string(255)
+#  name       :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
