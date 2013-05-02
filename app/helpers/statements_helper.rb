707d337af7e26cc5a223f77f771d2d360c48cfe1
Alex Hazoury <a.hazoury@gmail.com>
2013-05-02 11:09:57 +0100

    fixed ajax when voting for last statement

--- app/helpers/statements_helper.rb
+++ app/helpers/statements_helper.rb
@@ -3,4 +3,12 @@ module StatementsHelper
   def correct?
     @last_voted.true_or_false == @vote.vote ? 'Correct!' : 'Incorrect!'
   end
+
+  def last_statement?
+    if @next_statement == nil
+      'There are no statements left to vote on.  See your results on the left.'
+    else
+      link_to "Next Statement", statement_path(@next_statement), class: "next btn-small", remote: true
+    end
+  end
 end
